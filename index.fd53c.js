bcdc3b8926d007366134f0ee34eaf7fcbf924d6c
Isnur Margono <isnur.margono@shopee.com>
2023-06-19 15:47:24 +0700

    update

--- index.fd53c.js
+++ index.fd53c.js
@@ -1,4 +1,4 @@
-System.register(["./application.c140a.js"], function (_export, _context) {
+System.register(["./application.06f8e.js"], function (_export, _context) {
   "use strict";
 
   var createApplication, canvas, $p, bcr;
@@ -67,8 +67,8 @@ System.register(["./application.c140a.js"], function (_export, _context) {
   }
 
   return {
-    setters: [function (_applicationC140aJs) {
-      createApplication = _applicationC140aJs.createApplication;
+    setters: [function (_application06f8eJs) {
+      createApplication = _application06f8eJs.createApplication;
     }],
     execute: function () {
       canvas = document.getElementById('GameCanvas');
